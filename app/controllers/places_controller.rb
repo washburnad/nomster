be0486e2d256af44b534a8e83ee4f76681dfb9fe
Aaron Washburn <aaron@it-kutch.com>
2015-01-26 23:48:46 +0000

    Ensure correct user for edit and delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
 
 	def index
@@ -20,11 +20,18 @@ class PlacesController < ApplicationController
 	end
 
 	def edit
-		@place = Place.find(params[:id])
+	  @place = Place.find(params[:id])
+
+	  if @place.user != current_user
+	    return render :text => 'Not Allowed', :status => :forbidden
+	  end
 	end
 
 	def update
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+
 		@place.update_attributes(place_params)
 		redirect_to root_path
 	end
