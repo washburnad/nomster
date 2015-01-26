1ac8c024ca5a22a748bf8a28c9d50bfb34579e85
Aaron Washburn <aaron@it-kutch.com>
2015-01-26 22:14:16 +0000

    Add update place logic

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -24,7 +24,9 @@ class PlacesController < ApplicationController
 	end
 
 	def update
-
+		@place = Place.find(params[:id])
+		@place.update_attributes(place_params)
+		redirect_to root_path
 	end
 
 	private
