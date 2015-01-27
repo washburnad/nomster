d9a5b427f68463d735076d675d5647a62c0d1b0c
Aaron Washburn <aaron@it-kutch.com>
2015-01-27 11:23:02 +0000

    Add create and update validation and error styling

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+	  @place = current_user.places.create(place_params)
+	  if @place.valid?
+	    redirect_to root_path
+	  else
+	    render :new, :status => :unprocessable_entity
+	  end
 	end
 
 	def show
@@ -31,16 +35,21 @@ class PlacesController < ApplicationController
 		@place = Place.find(params[:id])
 		if @place.user != current_user
 			return render :text => 'Not Allowed', :status => :forbidden
-
+		end
 		@place.update_attributes(place_params)
-		redirect_to root_path
+
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, :status => :unprocessable_entity
+		end
 	end
 
 	def destroy
 		@place = Place.find(params[:id])
 		if @place.user != current_user
 			return render :text => 'Not Allowed', :status => :forbidden
-
+		end
 		@place.destroy
 		redirect_to root_path
 	end
