2cf757a6d1ab444e3f27129aff85bc76668b6d2f
Aaron Washburn <aaron@it-kutch.com>
2015-01-27 11:25:36 +0000

    Add more place validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :length => { :minimum => 3 }
+	validates :description, :presence => true
+	validates :address, :presence => true
 
 end
