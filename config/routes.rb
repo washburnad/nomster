39f9115eb0c12616c97a24230d2a4e23511d2641
Aaron Washburn <aaron@it-kutch.com>
2015-01-28 19:08:06 +0000

    Add photo create logic

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Nomster::Application.routes.draw do
   root 'places#index'
   resources :places do
     resources :comments, :only => :create
+    resources :photos, :only => :create
   end
 
   # Example of regular route:
