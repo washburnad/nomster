f508ec63490663e692193cf2b4ea9addc7d57c04
Aaron Washburn <aaron@it-kutch.com>
2015-01-29 03:25:52 +0000

    Send comment_added emails to real addresses

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-  	mail(to: "aaron@it-kutch.com",
+  	mail(to: "aarondalewashburn@gmail.com",
   		subject: "Comment was added")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,18 @@
 Nomster::Application.configure do
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+  
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -14,7 +28,7 @@ Nomster::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3030'}
 
   # Print deprecation notices to the Rails logger.
@@ -27,4 +41,5 @@ Nomster::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
 end
