3b686bad70d20cfbf44737076324197a3e788124
Aaron Washburn <aaron@it-kutch.com>
2015-01-29 11:30:12 +0000

    Add dynamic email addresses

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-  	mail(to: "aarondalewashburn@gmail.com",
-  		subject: "Comment was added")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+
+  	mail(to: @place_owner.email ,
+  		subject: "A comment was added to #{@place.name}")
   end
 end
